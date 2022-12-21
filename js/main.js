1da015a6d7c0648177e714ba192fc98682cab2a6
Yabrok <nosor321@mail.ru>
2022-12-21 12:21:02 +0500

    add search and sort

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
 var elForm = document.querySelector('.js-form');
 var elInput = document.querySelector('.js-input');
 var elList = document.querySelector('.js-list');
+var elSelect = document.querySelector('.js-select');
+var elSelect2 = document.querySelector('.js-select_2');
 elList.style.display = 'flex';
 elList.style.margin = '0';
 elList.style.padding = '0';
 
-let newArr = [];
-
-elForm.addEventListener('input', function (evt) {
-    evt.preventDefault();
-    elList.innerHTML = '';
-    var elInputValue = elInput.value.toLocaleLowerCase();
-    films.forEach((el) => {
-        if (el.title.toLocaleLowerCase().includes(elInputValue)) {
-            newArr.push(el)
-        };
-    });
-    render(newArr);
-    newArr = [];
-});
 
 
 function render(array) {
+    elList.innerHTML = '';
     for (var film of array) {
         var elItem = document.createElement('li');
-
         var filmPoster = document.createElement('img');
         filmPoster.setAttribute('src', film.poster);
         filmPoster.style.width = '100%'
@@ -50,5 +38,77 @@ function render(array) {
     }
 }
 
-render(films)
+render(films);
 
+// Select==========================================
+let newSet = new Set();
+films.forEach((item) => {
+    item.genres.forEach((type) => {
+        newSet.add(type)
+    })
+})
+
+newSet.forEach((gen) => {
+    let elOption = document.createElement('option');
+    elOption.value = gen;
+    elOption.textContent = gen;
+    elSelect.appendChild(elOption);
+});
+
+
+let newArr = [];
+elSelect.addEventListener('change', () => {
+    newArr = [];
+
+    if (elSelect.value != 'All') {
+        films.forEach((el) => {
+            if (el.genres.includes(elSelect.value)) {
+                newArr.push(el)
+                render(newArr)
+            };
+        });
+    } else {
+        render(films);
+    }
+});
+
+// ==============================================
+
+// Input==========================================
+
+let searchArr = []
+
+elForm.addEventListener('input', () => {
+    let elInputValue = elInput.value.toLocaleLowerCase();
+    films.forEach((x) => {
+        if (x.title.toLocaleLowerCase().includes(elInputValue)) {
+            searchArr.push(x);
+        };
+    })
+    render(searchArr);
+    searchArr = [];
+})
+// ====================================================
+
+// Sort=================================================
+
+elSelect2.addEventListener('change', () => {
+
+    if (elSelect2.value != '0'){
+        if (elSelect2.value == 'a-z') {
+            let aToz = films.sort((a, b) => {
+                return a.title.toLowerCase().charCodeAt(0) - b.title.toLowerCase().charCodeAt(0);
+            })
+            render(aToz);
+        };
+        if (elSelect2.value == 'z-a') {
+            let zToa = films.sort((a, b) => {
+                return b.title.toLowerCase().charCodeAt(0) - a.title.toLowerCase().charCodeAt(0);
+            })
+            render(zToa);
+        }
+    }else {
+        render(films);
+    }
+    
+})
